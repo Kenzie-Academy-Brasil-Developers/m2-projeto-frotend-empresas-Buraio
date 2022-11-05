e71dbcc6cf06e4a11270f33f37a4ac7986f09939
Bryan <bryanbatista79688@gmail.com>
2022-11-05 18:05:03 -0300

    merge

--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -1,11 +1,6 @@
 import { pageHref } from "../homePage/index.js";
-<<<<<<< HEAD
 import { redirectHeaderBtn } from "../../scripts/homePage/redirect.js";
 import { registerUser } from "../../scripts/register.js";
-=======
-import { redirectHeaderBtn } from "../../scripts/redirect.js";
-import { registerUser } from '../../scripts/register.js';
->>>>>>> develop
 
 pageHref.homePage = '../../../index.html';
 pageHref.login    = '../login/index.html';
